bbffff1fd3c8ec4bb9a91ee0c13aa00dc41a3daa
Alexandre Claro <alexandreclaro@MacBook-Air-de-Alexandre.local>
2023-07-25 18:47:06 -0300

    remocao de favicon e ajuste titulo

--- src/app/features/home-midia-kit/home-midia-kit.component.ts
+++ src/app/features/home-midia-kit/home-midia-kit.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+interface ComunidadeSelecionada {
+  nome: string;
+  codigo_estrutura: string;
+}
 
+interface GrupoSelecionado {
+  nome: string;
+  codigo_estrutura: string;
+  codigo_comunidade: string;
+}
+
+interface MeuObjeto {
+  comunidadesSelecionadas: ComunidadeSelecionada[];
+  gruposSelecionados: GrupoSelecionado[];
+}
 @Component({
   selector: 'home-midia-kit',
   templateUrl: './home-midia-kit.component.html',
   styleUrls: ['./home-midia-kit.component.scss'],
 })
-export class HomeMidiaKitComponent {}
+export class HomeMidiaKitComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}
